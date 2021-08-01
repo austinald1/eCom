5861b6213304ba62206572fb80eb34831c905915
austinald1 <81729807+austinald1@users.noreply.github.com>
2021-08-01 13:03:01 -0700

    configured the api routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,7 +4,7 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', async(req, res) => {
+router.get('/', async (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
   try {
@@ -15,13 +15,24 @@ router.get('/', async(req, res) => {
   } catch (err) {
     res.status(500).json(err);
   }
-
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const data = await Product.findByPk(req.params.id, {
+      include: [{ model: Category }, { model: Tag}],
+    });
+    if (!data) {
+      res.status(404).json({ message: "No products found using that id."});
+      return;
+    }
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
@@ -41,7 +52,6 @@ router.post('/', (req, res) => {
         const productTagIdArr = req.body.tagIds.map((tag_id) => {
           return {
             product_id: product.id,
-            tag_id,
           };
         });
         return ProductTag.bulkCreate(productTagIdArr);
@@ -94,12 +104,27 @@ router.put('/:id', (req, res) => {
     .then((updatedProductTags) => res.json(updatedProductTags))
     .catch((err) => {
       // console.log(err);
-      res.status(400).json(err);
+      res.status(200).json(err);
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const data = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if(!data) {
+      res.status(400).json({message: "Product id was not found, nothing has been deleted."})
+      return;
+    }
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json(err)
+  }
 });
 
 module.exports = router;
+
